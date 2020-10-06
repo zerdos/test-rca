d764fb6e4df7c928f95f816b345ed74b51ce4b3c
Zoltan Erdos <zolika84@gmail.com>
2020-10-06 13:13:45 +0000

    build

---
+++ precache-manifest.74cead0ab5de45235a699d482db34745.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "699bdc8d454c88d3b606f1e408e2709b",
+    "url": "/test-rca/index.html"
+  },
+  {
+    "revision": "8360d1a63fffe455ad54",
+    "url": "/test-rca/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aeb03fb996b59635f9d4",
+    "url": "/test-rca/static/js/2.3a5faa7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-rca/static/js/2.3a5faa7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8360d1a63fffe455ad54",
+    "url": "/test-rca/static/js/main.40b1b767.chunk.js"
+  },
+  {
+    "revision": "168c146915a835ec4c25",
+    "url": "/test-rca/static/js/runtime-main.e972fb83.js"
+  }
+]);
